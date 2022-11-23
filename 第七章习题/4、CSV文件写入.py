1a61940a846e6307ec50882ab040b54763e98966
Uyki <m15992167959@163.com>
2022-11-23 15:40:18 +0800

    Add files via upload

---
+++ 第七章习题/4、CSV文件写入.py
@@ -0,0 +1,24 @@
+l = [[]]
+# 第一行添加1到5
+for n in range(1, 6):
+    l[0].append(n)
+# 创建4个空列表
+for n in range(1, 5):
+    l.append([])
+# 往下添加数据
+    for i in range(1, 5):
+        l[n].append(l[n - 1][i])
+    # 末尾补数据
+    l[n].append(l[n - 1][0])
+# 输出矩阵
+for i in range(0,5):
+    for j in range(0,5):
+        print(l[i][j],end='  ')
+    print()
+
+# 写入矩阵
+with open('矩阵.csv','w') as fp:
+    for i in range(0,5):
+        for j in range(0,5):
+            fp.write(l[i][j])
+
